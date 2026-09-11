254ef350e726931f4b4ef65ff6ae2d4a89f151f2
Kesia Ferreira <kesiapimentaferreira525@gmail.com>
2026-09-11 12:40:59 -0300

    arquivos iniciais do projeto

---
+++ src/test/java/com/audiencia_virtual_bff/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audiencia_virtual_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
